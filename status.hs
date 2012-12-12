dd3bf64e3e73e1294b255019a5234c63079552d1
Chris Lindholm <chris.lindholm@gmail.com>
2012-12-12 00:08:45 -0700

    Changed to use STM and lenses. Only prints time for now. Doesn't yet wait, just runs constantly.

--- status.hs
+++ status.hs
@@ -1,56 +1,45 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Lens
 import Control.Monad (forever, sequence)
-import Data.ByteString.Char8 (unpack)
-import Data.Conduit
-import Data.Conduit.Binary (sourceFile, lines)
-import Data.Conduit.List (consume)
 import Data.Time
-import Prelude hiding (lines)
 import System.Cmd
 import System.Locale
-import System.Timer.Updatable
 
--- What if this was done by lazily constructing a string for the status
--- using the functions that get the data? That way, you can do all the
--- formatting easily, and then each time it tries to make the string, it
--- calls the functions and get the updated values.
+data Status = Status {_sTime :: String}
+
+instance Show Status where
+  show (Status t) = show t
+
+-- How does Template Haskell work?
+makeLenses ''Status
+
+-- TODO: Any way to do this automatically?
+emptyStatus :: Status
+emptyStatus = Status ""
 
 -- TODO: Need to run as soon as the program starts, then wait for the next
 -- time. Don't want to start by waiting.
 main :: IO ()
-main = forever $ serial (sequence thingsToDo >>= (\s ->
-  rawSystem "xsetroot" ["-name", unwords s])) (10^6) >>=
-    (\t -> waitIO t) >> return ()
+main = do
+  statusTVar <- atomically $ newTVar (Status "")
+  forever $ do
+    timeThread <- forkIO $ do
+      -- These lines can be combined w/ an applicative functor or
+      -- something like that.
+      time <- getTime
+      atomically $ modifyTVar' statusTVar $ (\x -> x & sTime .~ time)
+    status <- atomically $ readTVar statusTVar
+    putStatus $ show status
 
--- All the things used to construct the string will most likely
--- be in the IO monad.
-thingsToDo :: [IO String]
-thingsToDo = [getBattery, getBatteryStatus, getTime]
+putStatus :: String -> IO ()
+putStatus status = rawSystem "xsetroot" ["-name", status] >> return ()
 
--- TODO: Could this be done more idiomatically? (lift or something?)
--- TODO: Format so the time is only hours and minutes.
+-- TODO: Where should the formatting be done?
 getTime :: IO String
-getTime = getZonedTime >>=
-  (\t -> return $ formatTime defaultTimeLocale "%T" t)
-
--- TODO: How do I deal with ByteStrings?
-getBattery :: IO String
-getBattery = (runResourceT $ sourceFile battFile $= lines $$ consume) >>=
-  (\t -> return $ (unpack $ t !! 0) ++ "%")
-    where
-      battFile = "/sys/class/power_supply/BAT0/capacity"
-
--- TODO: Need to find out how to do this.
-{-
-getBatteryTime :: IO String
-getBatteryTime = undefined
--}
-
-getBatteryStatus :: IO String
-getBatteryStatus = (runResourceT $ sourceFile battFile $= lines $$ consume) >>=
-  (\t -> case (unpack $ t !! 0) of
-    "Full"     -> return "(=)"
-    "Charging" -> return "(+)"
-    _          -> return "(-)"
-  )
-    where
-      battFile = "/sys/class/power_supply/BAT0/status"
+getTime = formatT <$> getZonedTime
+  where
+    formatT = formatTime defaultTimeLocale "%T"
