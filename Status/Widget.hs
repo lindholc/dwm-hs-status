4b439edd7f136bfa808cd4a6641ee2032d3db91c
Chris Lindholm <chris.lindholm@gmail.com>
2013-03-08 16:57:35 -0700

    Added a missing file.

---
+++ Status/Widget.hs
@@ -0,0 +1,11 @@
+module Status.Widget
+  ( module Status.Widget.Audio
+  , module Status.Widget.Battery
+  , module Status.Widget.Time
+  , module Status.Widget.Wireless
+  ) where
+
+import Status.Widget.Audio
+import Status.Widget.Battery
+import Status.Widget.Time
+import Status.Widget.Wireless
